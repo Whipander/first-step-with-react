e2ff233cf4e64e22b4ae5b1025ef1dca12c02d04
Whipander <mihaja2506@gmail.com>
2025-07-03 16:16:26 +0300

    feat: add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
 
 function App() {
   return (
     <>
       <NavBar />
         <Hero/>
+        <About/>
     </>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,41 @@
+import aboutImage from '../assets/about.webp'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGraduationCap, faPeopleGroup, faMedal} from "@fortawesome/free-solid-svg-icons";
+
+function About() {
+    const textContent = [{
+        icon: faGraduationCap,
+        header: "+3 ans",
+        description: "Depuis 3 ans, nous formons ceux qui décodent le futur, aux métiers de l’informatique. Nous sommes un groupe d’experts, de professionnels et des passionnés de l’informatique de Madagascar et du monde entier."
+    }, {
+        icon: faPeopleGroup,
+        header: "+250 étudiants",
+        description: "Plus de 250 étudiants et plus d’une dizaine d’entreprises ont déjà fait confiance à notre formation habilitée par l’État malgache. Avec eux tous, nous sommes fiers de pouvoir parler de la “grande famille HEI”."
+    }, {
+        icon: faMedal,
+        header: "Notre mission",
+        description: "Notre mission est d’amener des jeunes malgaches sur des domaines porteurs comme l’intelligence artificielle, la cybersécurité ou la programmation afin de favoriser leur employabilité et le développement de Madagascar."
+    }]
+    return (<div className="h-screen flex justify-center items-center text-blue-950 py-2 mx-96 gap-10">
+        <div className={"flex justify-center items-center w-1/2"}>
+            <img src={aboutImage} alt="Hei students having fun" className={"w-[530px]"}/>
+        </div>
+        <div className={"flex flex-col gap-10 w-1/2"}>
+
+            <h2 className="text-5xl font-serif font-bold">À propos de nous</h2>
+
+            {textContent.map((item, index) => (
+                <div className={"flex flex-col gap-5 px-2"} key={index}>
+                    <div className={"flex items-center gap-2"}>
+                        <FontAwesomeIcon icon={item.icon} size={"4x"} className={"aspect-square text-amber-400"}/>
+                        <h3 className={"text-2xl font-bold"}>{item.header}</h3>
+                    </div>
+                    <p>{item.description}</p>
+                </div>
+            ))}
+
+        </div>
+    </div>)
+}
+
+export default About;
